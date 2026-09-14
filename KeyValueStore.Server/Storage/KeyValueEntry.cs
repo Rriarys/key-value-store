215e403291fa44f382a5f225c56c1614191a5f02
Rriarys <renegant.ex@gmail.com>
2026-09-14 22:06:25 +0300

    feat(storage): add in-memory key-value store with entry model. Close #2

---
+++ KeyValueStore.Server/Storage/KeyValueEntry.cs
@@ -0,0 +1,7 @@
+﻿namespace KeyValueStore.Server.Storage;
+
+internal class KeyValueEntry
+{
+    public string Key { get; set; } = null!;
+    public string Value { get; set; } = null!;
+}
---
+++ KeyValueStore.Server/Storage/KeyValueStore.cs
@@ -0,0 +1,33 @@
+﻿namespace KeyValueStore.Server.Storage;
+
+// Storage for KeyValueEntry objects
+internal class KeyValueStore
+{
+    private readonly Dictionary<string, KeyValueEntry> _store = new();
+    public void Set(KeyValueEntry entry)
+    {
+        _store[entry.Key] = entry;
+    }
+    public bool Exists(string key)
+    {
+        return _store.ContainsKey(key); // just if exists, not returning the value
+    }
+    public KeyValueEntry? TryGet(string key)
+    {
+        _store.TryGetValue(key, out var entry);
+        return entry; // returns the value if exists
+    }
+    public bool Update(string key, string value)
+    {
+        if (_store.ContainsKey(key))
+        {
+            _store[key].Value = value;
+            return true;
+        }
+        return false;
+    }
+    public bool Delete(string key)
+    {
+        return _store.Remove(key);
+    }
+}
